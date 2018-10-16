f271d972322c455c7bdc9de248cccc9bd0c9a93a
jwk <jw99@daou.co.kr>
2018-10-16 15:55:02 +0900

    ui작성중

--- htdocs/application/view/board/read_form.php
+++ htdocs/application/view/board/read_form.php
@@ -10,7 +10,13 @@
     게시판
 </div>
 <div class="container">
-    <div> 내용</div>
+    <span> 제목 : </span> 반갑습니다
+    <hr>
+    <div style="text-align: right;">
+    <span> 작성자 : </span> 김지웅 <span>작성일 : </span> 10:00
+    </div>
+    <hr>
+    <div class="contents">안녕하세요</div>
 </div>
 </body>
 </html>
